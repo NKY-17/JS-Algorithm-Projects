1d93439d51b25164d53f80b3e5756b04ee049bc8
NKY-17 <nujahirkamal17@gmail.com>
2026-01-24 00:52:32 +0600

    cash-register.js

--- cash-register.js
+++ cash-register.js
@@ -1,4 +1,4 @@
-** start of script.js **
+//** start of script.js **
 
 function checkCashRegister(price, cash, cid) {
   const currencyUnits = [
@@ -55,5 +55,5 @@ function checkCashRegister(price, cash, cid) {
 
 checkCashRegister(19.5, 20, [["PENNY", 1.01], ["NICKEL", 2.05], ["DIME", 3.1], ["QUARTER", 4.25], ["ONE", 90], ["FIVE", 55], ["TEN", 20], ["TWENTY", 60], ["ONE HUNDRED", 100]]);
 
-** end of script.js **
+//** end of script.js **
 
